240486df6ac7c456a774c19cbc506e4b5afc2542
Hasan Goli <mrhasangoli@gmail.com>
2025-02-16 22:46:49 +0330

    Theme instance initialized

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Characters"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text("Character List"),
+            Text(
+              "Character List",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              "Character List",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: Text("Create New"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
